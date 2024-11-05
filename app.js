1a30b952d88abffc28e208341a2cf01875c46b40
M0KR1 <coolcreeper9@gmail.com>
2024-11-05 13:02:43 +0400

    Add files via upload

--- app.js
+++ app.js
@@ -7,13 +7,14 @@ let item = "";
 let btn1 = document.getElementById("btn1");
 let btn2 = document.getElementById("btn2");
 let btn3 = document.getElementById("btn3");
+let btn4 = document.getElementById("btn4");
 
 btn1.addEventListener("click", function () {
     if (tg.MainButton.isVisible) {
         tg.MainButton.hide();
     }
     else {
-        tg.MainButton.setText("Вывести информацию по жанру комедии");
+        tg.MainButton.setText("Вывести информацию по жанру Футбол");
         item = "1";
         tg.MainButton.show();
     }
@@ -24,19 +25,30 @@ btn2.addEventListener("click", function () {
         tg.MainButton.hide();
     }
     else {
-        tg.MainButton.setText("Вывести информацию по жанру драме");
+        tg.MainButton.setText("Вывести информацию по жанру Баскетбол");
         item = "2";
         tg.MainButton.show();
     }
 });
 
 btn3.addEventListener("click", function () {
-	if (tg.MainButton.isVisible) {
+    if (tg.MainButton.isVisible) {
         tg.MainButton.hide();
     }
-	else {
-		tg.MainButton.setText("Вывести информацию по жанру приключения");
+    else {
+        tg.MainButton.setText("Вывести информацию по жанру Волейбол");
         item = "3";
+        tg.MainButton.show();
+    }
+});
+
+btn4.addEventListener("click", function () {
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide();
+    }
+    else {
+        tg.MainButton.setText("Вывести информацию по жанру Настольный теннис");
+        item = "4";
         tg.MainButton.show();
     }
 });
